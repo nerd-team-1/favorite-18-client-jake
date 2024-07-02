8743a5ca5762bce3174bdb1a76a583ab2eabdc68
jake <epikeran@gmail.com>
2024-07-03 00:12:46 +0900

    feat : Login screen with hook and validate

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,14 +1,72 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useRef} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import CustomInputField from '../../components/CustomInputField';
+import CustomButton from '../../components/CustomButton';
+import useForm from '../../hooks/useForm';
+import {validateLogin} from '../../utils';
+import {TextInput} from 'react-native-gesture-handler';
 
 function LoginScreen() {
+  const passwordRef = useRef<TextInput | null>(null);
+
+  const login = useForm({
+    initialValue: {
+      email: '',
+      password: '',
+    },
+    validate: validateLogin,
+  });
+
+  const handleSubmit = () => {
+    console.log(login.values);
+  };
+
   return (
-    <View>
-      <Text>로그인 스크린</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <CustomInputField
+          autoFocus
+          placeholder="이메일"
+          error={login.errors.email}
+          touched={login.touched.email}
+          inputMode="email"
+          returnKeyType="next"
+          blurOnSubmit={false}
+          onSubmitEditing={() => passwordRef.current?.focus()}
+          {...login.getTextInputProps('email')}
+        />
+        <CustomInputField
+          ref={passwordRef}
+          placeholder="비밀번호"
+          error={login.errors.password}
+          touched={login.touched.password}
+          secureTextEntry
+          returnKeyType="join"
+          blurOnSubmit={false}
+          onSubmitEditing={handleSubmit}
+          {...login.getTextInputProps('password')}
+        />
+      </View>
+      <CustomButton
+        label="로그인"
+        variant="filled"
+        size="large"
+        onPress={handleSubmit}
+      />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 30,
+  },
+});
 
 export default LoginScreen;
