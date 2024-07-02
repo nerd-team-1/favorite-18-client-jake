748a29eddd894b21950f27d1a7cda5015db71ea8
jake <epikeran@gmail.com>
2024-07-02 23:49:12 +0900

    feat : Custom input field

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,8 +1,13 @@
 const colors = {
-  WHITE: '#fff',
-  BLACK: '#000',
   PRIMARY_LIGHT: '#4374D9',
   PRIMARY: '#0054FF',
+  RED_300: '#FFB4B4',
+  RED_500: '#FF5F5F',
+  WHITE: '#fff',
+  GRAY_200: '#E7E7E7',
+  GRAY_500: '#8E8E8E',
+  GRAY_700: '#575757',
+  BLACK: '#000',
 } as const;
 
 export {colors};
