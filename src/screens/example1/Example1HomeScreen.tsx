53c22065723a2adeed038e0a43c256da1bed681a
jake <epikeran@gmail.com>
2024-07-10 20:35:08 +0900

    chore : Set env config

--- src/screens/example1/Example1HomeScreen.tsx
+++ src/screens/example1/Example1HomeScreen.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import Config from 'react-native-config';
 
 function Example1HomeScreen() {
+  console.log('env.test', Config.TEST);
+
   return (
     <View>
       <Text>예제 스크린1</Text>
